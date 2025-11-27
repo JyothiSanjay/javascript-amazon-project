f5bc7cc5fa5b1ffa9023a0202f006d277d0af5f3
Jyothi S <jyothih80@gmail.com>
2025-11-27 10:25:28 +0530

    utils

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/currencyformat.js';
 
 cart.forEach(cartItem => {
     const product = products.find(p => p.id === cartItem.productId);
@@ -18,7 +19,7 @@ cart.forEach(cartItem => {
                   ${product.name}
                 </div>
                 <div class="product-price">
-                  $${(product.priceCents / 100).toFixed(2)}
+                   $${formatCurrency(product.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -40,7 +41,7 @@ cart.forEach(cartItem => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -53,7 +54,7 @@ cart.forEach(cartItem => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -66,7 +67,7 @@ cart.forEach(cartItem => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
